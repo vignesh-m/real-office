e6daa0f42456fdde26a79b1a5451d097bfdcb6e2
vignesh-m <vicky.mano@gmail.com>
2017-03-18 12:31:30 +0530

    added create form, view

--- meetings/views.py
+++ meetings/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.forms import ModelForm
 
-# Create your views here.
+from .models import Meeting
 
 
 def index(request):
     return HttpResponse("Hi")
+
+
+class MeetingForm(ModelForm):
+
+    class Meta:
+        model = Meeting
+        exclude = ['creatingStaff']
+
+
+def create(request):
+    if request.method == 'POST':
+        form = MeetingForm(request.POST)
+        print(form)
+        return HttpResponse('Meeting Created!')
+    else:
+        form = MeetingForm()
+        return render(request, 'create_meeting.html', {'form': form})
