54a391ef617a114193a087e05502ab105542cb71
vignesh-m <vicky.mano@gmail.com>
2017-03-27 17:04:04 +0530

    added host

--- realoffice/settings.py
+++ realoffice/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'k)xqdsm%rv48v7^&r%de7r-!1nxm$@cf@=n5b(u5-17%8yo_nb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'vigneshm.pythonanywhere.com'
+]
 
 
 # Application definition
