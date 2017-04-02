cea2bc3b828ed6e10bd3d30c87ff98cb1191b469
premkrishnaa <premkrishnaa1996@gmail.com>
2017-04-02 16:05:16 +0530

    login optimised

--- realoffice/views.py
+++ realoffice/views.py
@@ -6,22 +6,32 @@ from django.contrib import messages
 
 def login_view(request):
     if request.method == 'GET':
+        next1 = request.GET.get('next')
+        if(next1 == None):
+            next1 = '/'
+        error = request.GET.get('error')
+        if(error == None):
+            error = 'nil'
         for m in messages.get_messages(request):
             print(m)
-        return render(request, 'login.html')
+        return render(request, 'login.html', {'next': next1, 'error': error})
+
     else:
-        print(request.POST)
+        # print(request.POST)
         username = request.POST['username']
         password = request.POST['password']
+        next1 = request.POST['next']
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
         else:
             message = 'Incorrect Credentials'
-            return render(request, 'login.html', {'error': message})
-        return HttpResponseRedirect('/')
+            url = '/login/?next='+next1+'&'+'error='+message
+            return HttpResponseRedirect(url)
+
+        return HttpResponseRedirect(next1)
 
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect('/login')
+    return HttpResponseRedirect('/login/?next=/')
